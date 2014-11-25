4841a045791fdf332b1d0e2843171431f74f486f
andyboydston <andyboydston@gmail.com>
2014-11-25 12:06:56 -0800

    no message

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,24 @@
 
 'use strict';
 
+// var http require('http');
+
+// var server = http.createServer(function(req, res) {
+// 	res.writeHead(200, {'Content-Type': 'text/plain'});
+// 	res.end('Hello World from Andy!\n');
+// });
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hell Word from Dave!');
+});
+
+app.get('/foo', function(req, res) 
+	res.send('You requested /foo!');
+});
+
+app.listen(8080, function() {
+	console.log('server listening on http://localhost:8080');
+});
